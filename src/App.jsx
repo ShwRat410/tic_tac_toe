4530a87c31ed28babb79647bcbaa19d7908ab0fa
ShwRat410 <shwetaratnawat410@gmail.com>
2025-02-13 11:40:08 +0000

    Basic structure

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,28 @@ function App() {
   
 
   return (
-    <h1>React Tic-Tac-Toe</h1>
+  <main>
+    <div id="game-container">
+      <div>
+        <ol id="players">
+          <li>
+            <span className="player-name">Player1</span>
+            <span className="player-symbol">X</span>
+          </li>
+          <li>
+            <span className="player-name">Player2</span>
+            <span className="player-symbol">O</span>
+          </li>
+        </ol>
+      </div>
+      <div>
+        GameBoard
+      </div>
+    </div>
+    <div>
+      Log
+    </div>
+  </main>
   )
 }
 
